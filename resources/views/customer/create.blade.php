05cef9a975bbeaeee33eb9620a65ac71bdc5d8af
Rasnomawan <alvinorasya9@gmail.com>
2025-07-13 09:37:35 +0700

    Initial commit

---
+++ resources/views/customer/create.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="card">
+            <form action="{{  }}" method="post">
+                @csrf
+
+                <div>
+                    <label for="">Costumer name</label>
+                    <input type="text" name="" id="">
+                </div>
+                <div>
+                    <label for="">Email</label>
+                    <input type="text" name="email" class="form-control">
+                </div>
+                <div>
+                    <button class="btn bg-success text-white">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/customer/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="card">
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Name</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+@endsection
